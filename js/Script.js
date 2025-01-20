a8f4a0ef63e9be834abc7a9118a4a3ba463c33d4
antoineBondet <91895104+antoineBondet@users.noreply.github.com>
2025-01-20 12:14:48 +0100

    article v1

--- js/Script.js
+++ js/Script.js
@@ -1,1 +1,18 @@
-console.log('Hello World');
+const buttonAjouterPanier1 = document.getElementById("btn1");
+const buttonAjouterPanier2 = document.getElementById("btn2");
+const buttonAjouterPanier3 = document.getElementById("btn3");
+console.log(buttonAjouterPanier1);
+console.log(buttonAjouterPanier2);
+console.log(buttonAjouterPanier3);
+
+buttonAjouterPanier1.addEventListener('click', () => {
+    alert('Article  1 ajouté au panier');
+});
+
+buttonAjouterPanier2.addEventListener('click', () => {
+    alert('Article  2 ajouté au panier');
+});
+
+buttonAjouterPanier3.addEventListener('click', () => {
+    alert('Article 3 ajouté au panier');
+});
